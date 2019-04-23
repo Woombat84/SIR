c855b98685529c9210b5d18b2653cf4217e8e210
Mark Richard Blankensteiner <mblank16@student.aau.dk>
2019-04-23 14:23:36 +0200

    Got header compliling

--- Project1/prossecing.cpp
+++ Project1/prossecing.cpp
@@ -17,26 +17,26 @@ prossecing::~prossecing()
 }
 
 
-prossecing::vectorSizing(const std::vector &A, const std::vector B) {
-	std::template <std::typename T>
-	std::vector<T> operator+(const std::vector<T> A, const std::vector<T> &B)
-	{
-		std::vector<T> AB;
-		AB.reserve(A.size() + B.size());                // preallocate memory
-		AB.insert(AB.end(), A.begin(), A.end());        // add A;
-		AB.insert(AB.end(), B.begin(), B.end());        // add B;
-		return AB;
-	}
-}
-prossecing::isIn(cv::Point myPoint)
+
+template <typename T>
+std::vector<T>operator+(const std::vector<T> &A, const std::vector<T> &B)
 {
-	for (int i = 0; i < std::my_contours.size(); i++)
-		if (std::find(std::my_contours[i].begin(), my_contours[i].end(), myPoint) != my_contours[i].end())
+	std::vector<T> AB;
+	AB.reserve(A.size() + B.size());                // preallocate memory
+	AB.insert(AB.end(), A.begin(), A.end());        // add A;
+	AB.insert(AB.end(), B.begin(), B.end());        // add B;
+	return AB;
+}
+
+int prossecing::isIn(cv::Point myPoint)
+{
+	for (int i = 0; i < my_contours.size(); i++)
+		if (find(my_contours[i].begin(), my_contours[i].end(), myPoint) != my_contours[i].end())
 			return 1;
 	return 0;
 }
 
-prossecing::rob_findContours(cv::Mat img, int treshold)
+void prossecing::rob_findContours(cv::Mat &img, int treshold)
 
 {
 	//	img = contrast(img, 1);
@@ -56,18 +56,18 @@ prossecing::rob_findContours(cv::Mat img, int treshold)
 
 
 
-prossecing::rob_findCountor(cv::Mat img, cv::Point myPoint, std::int tresholdValue)
+void prossecing::rob_findCountor(cv::Mat &img, cv::Point myPoint,int tresholdValue)
 
 {
 	//	Point newPoint;
-	int dif = 1;
-	std::my_vector.push_back(myPoint);
+	
+	my_vector.push_back(myPoint);
 	if ((myPoint.x >= dif || myPoint.x < img.cols - dif) && (myPoint.y >= dif || myPoint.y < img.rows - dif))
 	{
 		for (int i = -dif; i <= dif; i++)
 			for (int j = -dif; j <= dif; j++)
 			{
-				Point newPoint{ myPoint.x + i, myPoint.y + j };
+				cv::Point newPoint{ myPoint.x + i, myPoint.y + j };
 
 				if ((abs(img.at<uchar>(newPoint) - img.at<uchar>(myPoint)) >= tresholdValue) && (find(my_vector.begin(), my_vector.end(), newPoint) == my_vector.end()))
 					rob_findCountor(img, newPoint, tresholdValue);
@@ -76,7 +76,7 @@ prossecing::rob_findCountor(cv::Mat img, cv::Point myPoint, std::int tresholdVal
 }
 
 
-prossecing::drawImage(cv::Mat img)
+void prossecing::drawImage(cv::Mat &img)
 {
 	for (int i = 0; i < img.rows; i++)
 		for (int j = 0; j < img.cols; j++)
@@ -92,7 +92,7 @@ prossecing::drawImage(cv::Mat img)
 
 }
 
-prossecing::threshold(){
+void prossecing::threshold(void *asf){
 
 
 
--- Project1/prossecing.h
+++ Project1/prossecing.h
@@ -10,12 +10,13 @@ class prossecing
 public:  //Methodes
 	prossecing();
 	~prossecing();
-	void vectorSizing();
+
 	int isIn(cv::Point myPoint);
-	void rob_findCountor(cv::Mat img, cv::Point myPoint, int tresholdValue);
-	void rob_findContours(cv::Mat img, int treshold);
-	void drawImage(cv::Mat img);
+	void rob_findCountor(cv::Mat& img, cv::Point myPoint, int tresholdValue);
+	void rob_findContours(cv::Mat& img, int treshold);
+	void drawImage(cv::Mat& img);
 	void threshold(void*);
+	
 
 private: //Methodes
 
@@ -25,7 +26,9 @@ public: // members
 	
 
 private: //members
+	int dif = 1;
 	std::vector<cv::Point>	my_vector;
 	std::vector<std::vector<cv::Point>>  my_contours;
+	
 };
 
