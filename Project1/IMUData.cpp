ac945a388abc54decbed85a71b492fc609b44510
Mark Richard Blankensteiner <mblank16@student.aau.dk>
2019-04-23 18:13:25 +0200

    Complimentray filter added

--- Project1/IMUData.cpp
+++ Project1/IMUData.cpp
@@ -4,14 +4,53 @@
 #include <opencv2\imgproc.hpp>
 #include <iostream>
 #include "IMUData.h"
-
-
-
+#include <math.h>
+#include <chrono>
+#include<stdlib.h>
 IMUData::IMUData()
 {
+	 oldRollTime = std::chrono::steady_clock::now();
+	 oldPitchTime = std::chrono::steady_clock::now();
 }
 
 
 IMUData::~IMUData()
 {
 }
+// Complimentary filtering of roll
+float IMUData::Roll(float Xgyro)
+{
+	rollTime = std::chrono::steady_clock::now();
+	float rT = float(std::chrono::duration_cast< std::chrono::nanoseconds> (rollTime - oldRollTime).count());
+	rT = rT / 1e9;
+	float roll = (xzDeg*compAccel)+((Xgyro * rT) *compGyro);
+	oldRollTime = std::chrono::steady_clock::now();
+	return roll;
+}
+// Complimentery filtering of pitch
+float IMUData::Pitch(float Ygyro)
+{	
+	pitchTime = std::chrono::steady_clock::now();
+	float pT = float(std::chrono::duration_cast<std::chrono::nanoseconds>(pitchTime - oldPitchTime).count());
+	pT = pT / 1e9;
+	float pitch = (yzDeg * compAccel) + ((Ygyro * pT) * compGyro);
+	oldPitchTime = std::chrono::steady_clock::now();
+	return pitch;
+}
+void IMUData::setCompFliter(float S)
+{
+	compAccel = S;
+
+
+}
+// Calculating the rotation of YZ plane
+void IMUData::YZdeg(float& Y, float& Z)
+{
+	yzDeg = sin(abs(Z) / sqrtf(Y * Y + Z * Z));
+}
+// Calculating the rotaion  of XZ plane
+void IMUData::XZdeg(float& X, float& Z)
+{
+	xzDeg = sin(abs(Z) / sqrtf(X * X + Z * Z));
+}
+
--- Project1/IMUData.h
+++ Project1/IMUData.h
@@ -1,17 +1,33 @@
 #pragma once
+#include <math.h>
+#include <chrono>
+
 class IMUData
 {
 public:
 	IMUData();
 	~IMUData(); 
-
+	float Roll(float Xgyro);
+	float Pitch(float Ygyro);
+	void setCompFliter(float S);
 
 private: // methodes
+	void YZdeg(float& Y, float& Z);
+	void XZdeg(float& X, float& Z);
 
 
 public:  // members
 
 
 private: // members
+	float yzDeg;
+	float xzDeg;
+	float compAccel = 0.05f;
+	float compGyro = 1 - compAccel;
+	std::chrono::steady_clock::time_point rollTime;
+	std::chrono::steady_clock::time_point pitchTime;
+	std::chrono::steady_clock::time_point oldRollTime;
+	std::chrono::steady_clock::time_point oldPitchTime;
+
 };
 
