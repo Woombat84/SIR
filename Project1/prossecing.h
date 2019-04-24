ce3aadac63cbf9c61baa923803f457651ea37a51
ValdemarQvist <33546601+ValdemarQvist@users.noreply.github.com>
2019-04-24 11:35:42 +0200

    added color to greyscale

--- Project1/prossecing.h
+++ Project1/prossecing.h
@@ -11,7 +11,8 @@ public:  //Methodes
 	void rob_findCountor(cv::Mat& img, cv::Point myPoint, int tresholdValue);
 	void rob_findContours(cv::Mat& img, int treshold);
 	void drawImage(cv::Mat& img);
-	void threshold(void*);
+	void greyscale(cv::Mat& img);
+	void threshold(cv::Mat& img);
 	
 
 private: //Methodes
@@ -23,6 +24,10 @@ public: // members
 
 private: //members
 	int dif = 1;
+	int threshThreshold = 60;
+	double wr = 0.3;
+	double wb = 0.3;
+	double wg = 0.3;
 	std::vector<cv::Point>	my_vector;
 	std::vector<std::vector<cv::Point>>  my_contours;
 	
