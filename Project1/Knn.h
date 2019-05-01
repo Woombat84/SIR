f50d5649915421bfb0a5093fe7d3849ebdd85aa5
MihaiGaidau <gaidaumihai@gmail.com>
2019-05-01 12:16:44 +0200

    knnAndOtherFunctions

--- Project1/Knn.h
+++ Project1/Knn.h
@@ -4,9 +4,14 @@ class Knn
 public: // methodes
 	Knn();
 	~Knn();
+	std::string rob_knn(std::vector<float> testVector, std::vector<std::vector<float>> trainingData, std::vector<std::string> classes, int k);
+
+
 
 private: // methodes
-
+	void heapify(std::vector<float>& myVector, std::vector<int>& clInd, int n, int i);
+	void heapSort(std::vector<float>& myVector, std::vector<int>& clInd);
+	int string_pos(std::vector<std::string> myVector, std::string myString);
 
 public:  // members
 
