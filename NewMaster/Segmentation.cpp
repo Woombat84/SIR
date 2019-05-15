5b8677496f63ab01d1e2596d842cdc56c497ac6f
Jonathan Schmidt <jschmi17@student.aau.dk>
2019-05-15 12:54:21 +0200

    Inverse Threshold

--- NewMaster/Segmentation.cpp
+++ NewMaster/Segmentation.cpp
@@ -63,10 +63,10 @@ void Segmentation::Threshold(cv::Mat& img, int Threshold) {
 	for (int x = 0; x < img.cols; x++) {
 		for (int y = 0; y < img.rows; y++) {
 			if (img.at <uchar>(y, x) > Threshold) { //If value is above threshold make white
-				img.at<uchar>(y, x) = 255;
+				img.at<uchar>(y, x) = 0;
 			}
 			else {
-				img.at<uchar>(y, x) = 0; //Otherwise make pixel black
+				img.at<uchar>(y, x) = 255; //Otherwise make pixel black
 			}
 		}
 	}
